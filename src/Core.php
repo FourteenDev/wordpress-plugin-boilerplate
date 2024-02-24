4722484da0e91d350f27c26de0b7b7c49b20b3f9
Koorosh14 <97566920+Koorosh14@users.noreply.github.com>
2024-02-24 15:49:16 +0330

    add comments to url() and dir() methods

--- src/Core.php
+++ src/Core.php
@@ -14,12 +14,26 @@ class Core
 
 	public function __construct() {}
 
-	public function url($path = null)
+	/**
+	 * Returns plugin's URL path, without any slashes in the end (e.g. `https://Site.com/wp-content/plugins/my-plugin`).
+	 *
+	 * @param	string	$path	Path to append to the end of the URL, without any slashes in the beginning (e.g. `path/to/my-file.php`).
+	 *
+	 * @return	string
+	 */
+	public function url($path = '')
 	{
 		return untrailingslashit(FDWPBP_URL . $path);
 	}
 
-	public function dir($path = null)
+	/**
+	 * Returns plugin's dir path, without any slashes in the end (e.g. `/var/www/html/wp-content/plugins/my-plugin`).
+	 *
+	 * @param	string	$path	Path to append to the end of the dir, without any slashes in the beginning (e.g. `path/to/my-file.php`).
+	 *
+	 * @return	string
+	 */
+	public function dir($path = '')
 	{
 		return untrailingslashit(FDWPBP_DIR . $path);
 	}
