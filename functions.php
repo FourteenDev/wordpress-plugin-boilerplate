dea0a0d4b243904d4688bd5096ab32b38a0c4f88
Koorosh14 <97566920+Koorosh14@users.noreply.github.com>
2024-02-24 20:46:40 +0330

    small (unimportant) reformat in comments

--- functions.php
+++ functions.php
@@ -4,18 +4,18 @@
  * Returns current plugin's metadata.
  *
  * @return	array	Plugin data listed below (*Values will be empty if not supplied by the plugin*):
- * - `Name` 		=> *(string)* Name of the plugin. Should be unique.
- * - `Title` 		=> *(string)* Title of the plugin and link to the plugin's site (if set).
- * - `Description` 	=> *(string)* Plugin description.
- * - `Author` 		=> *(string)* Author's name.
- * - `AuthorURI` 	=> *(string)* Author's website address (if set).
- * - `Version` 		=> *(string)* Plugin version.
- * - `TextDomain` 	=> *(string)* Plugin textdomain.
- * - `DomainPath` 	=> *(string)* Plugins relative directory path to .mo files.
- * - `Network` 		=> *(bool)* Whether the plugin can only be activated network-wide.
- * - `RequiresWP` 	=> *(string)* Minimum required version of WordPress.
- * - `RequiresPHP` 	=> *(string)* Minimum required version of PHP.
- * - `UpdateURI` 	=> *(string)* ID of the plugin for update purposes, should be a URI.
+ * - `Name`        => *(string)* Name of the plugin. Should be unique.
+ * - `Title`       => *(string)* Title of the plugin and link to the plugin's site (if set).
+ * - `Description` => *(string)* Plugin description.
+ * - `Author`      => *(string)* Author's name.
+ * - `AuthorURI`   => *(string)* Author's website address (if set).
+ * - `Version`     => *(string)* Plugin version.
+ * - `TextDomain`  => *(string)* Plugin textdomain.
+ * - `DomainPath`  => *(string)* Plugins relative directory path to .mo files.
+ * - `Network`     => *(bool)* Whether the plugin can only be activated network-wide.
+ * - `RequiresWP`  => *(string)* Minimum required version of WordPress.
+ * - `RequiresPHP` => *(string)* Minimum required version of PHP.
+ * - `UpdateURI`   => *(string)* ID of the plugin for update purposes, should be a URI.
  */
 function fdwpbpGetPluginData()
 {
