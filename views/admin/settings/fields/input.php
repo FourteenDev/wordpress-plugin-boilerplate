09d9e0cc8b14dcce70259cd3bff011ec382a465e
Koorosh14 <97566920+Koorosh14@users.noreply.github.com>
2024-11-06 21:54:06 +0330

    Fix checkbox issue

--- views/admin/settings/fields/input.php
+++ views/admin/settings/fields/input.php
@@ -1,4 +1,8 @@
-<input type="<?php echo esc_attr($type); ?>" id="<?php echo esc_attr($id); ?>" name="<?php echo esc_attr($name); ?>" value="<?php echo $type === 'number' ? intval($value) : esc_attr($value); ?>" <?php echo $type === 'checkbox' ? checked($value, true, false) : ''; ?> />
+<?php
+$valueAttribute = $type === 'number' ? intval($value) : esc_attr($value);
+$valueAttribute = $type === 'checkbox' ? '1' : $valueAttribute;
+?>
+<input type="<?php echo esc_attr($type); ?>" id="<?php echo esc_attr($id); ?>" name="<?php echo esc_attr($name); ?>" value="<?php echo $valueAttribute; ?>" <?php echo $type === 'checkbox' ? checked($value, true, false) : ''; ?> />
 <?php if (!empty($description)) : ?>
 	<p><?php echo $description; ?></p>
 <?php endif; ?>
