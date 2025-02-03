3943a8789695a2bd9a12d1a93f10f530c9154212
Koorosh14 <97566920+Koorosh14@users.noreply.github.com>
2025-02-03 15:51:05 +0330

    Improve Model class

--- src/Model.php
+++ src/Model.php
@@ -6,7 +6,11 @@ class Model
 {
 	public static $instance = null;
 
+	/** @var	\wpdb */
 	private $wpdb;
+
+	/** @var	string */
+	private $charsetCollate;
 
 	public static function getInstance()
 	{
@@ -17,7 +21,9 @@ class Model
 	public function __construct()
 	{
 		global $wpdb;
-		$this->wpdb = $wpdb;
+
+		$this->wpdb           = $wpdb;
+		$this->charsetCollate = $wpdb->get_charset_collate();
 
 		// Example database related method:
 		// add_action('plugins_loaded', [$this, 'initializeTables']);
@@ -34,7 +40,14 @@ class Model
 	{
 		if (trim(get_option(FDWPBP_OPTIONS_KEY_DB_VERSION, '')) === trim(FDWPBP_VERSION)) return;
 
+		require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+
 		// Example query:
+		/* dbDelta("CREATE TABLE `{$this->wpdb->prefix}some_table` (
+			...
+		) {$this->charsetCollate};"); */
+
+		// or maybe: 
 		/* $this->wpdb->query(
 			"CREATE TABLE IF NOT EXISTS `{$this->wpdb->prefix}some_table` (
 				...
